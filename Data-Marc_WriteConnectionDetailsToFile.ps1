fa5e3aaa5f39b52e1d5eb9bfe860d73b02da5f90
Marc Lelijveld <38921736+marclelijveld@users.noreply.github.com>
2020-08-12 11:15:18 +0200

    Version 1.0.1 with minor changes
    External Tools Power BI model documentor script version 1.0.1
    
    New in this version: 
    - Easier editing of personal preferred location. This is now defined in one single variable ($defaultlocation). 
    - Added logging for the script. 
    - Prepared for upcoming changes with $finalLocation. Soon more about this.
    - Better error handling in the pbit file which results in less errors while loading the data.

--- Data-Marc_WriteConnectionDetailsToFile.ps1
+++ Data-Marc_WriteConnectionDetailsToFile.ps1
@@ -1,8 +1,26 @@
-﻿# Below you can define your personal preference for file saving and reading. 
-# The outputfolder will be used to dump a temporarily file with the connection to the model. 
+﻿<# 
+    External Tools Power BI model documentor script version 1.0.1
+    New in this version: 
+        - Easier editing of personal preferred location. This is now defined in one single variable ($defaultlocation). 
+        - Added logging for the script. 
+        - Prepared for upcoming changes with $finalLocation. Soon more about this.
+        - Better error handling in the pbit file which results in less errors while loading the data. 
+
+    Full change log can be found here: https://data-marc.com/model-documenter/
+#>
+
+# Below you can define your personal preference for file saving and reading. 
+# The default location can be changed and will be leverages througout the entire script. 
+$defaultLocation = 'c:\temp\'
+$finalLocation = $defaultLocation
+
+#This part starts tracing to catch unfortunate errors and defines where to write the file. 
+$Logfile = $finalLocation + 'PBI_DocumentModel_LogFile.txt'
+Start-Transcript -Path $Logfile
+
 # The PBITLocation defines where the templated PBIT file is saved. 
-$OutputFolder = 'c:\temp\'
-$PBITLocation = 'C:\temp\ModelDocumentationTemplate.pbit'
+# In case you are using a different pbit file, you can define that in below variable.
+$PBITLocation = $finalLocation + 'ModelDocumentationTemplate.pbit'
 
 # Below section defines the server and databasename based on the input captured from the External tools integration. 
 # This is defined as arguments \"%server%\" and \"%database%\" in the external tools json. 
@@ -22,8 +40,14 @@ $json = @"
 "@
 
 # Writes the output in json format to the defined file location. This is a temp location and will be overwritten next time. 
-$OutputLocation = $OutputFolder + 'ModelDocumenterConnectionDetails.json'
+$OutputLocation = $finalLocation + 'ModelDocumenterConnectionDetails.json'
 $json  | ConvertTo-Json  | Out-File $OutputLocation
 
 # Open PBIT template file from PBITLocation as defined in the variable. 
 Invoke-Item $PBITLocation
+
+# Catching possible errors and writing to the log file
+Write-Output 'Errors occured: ' $error.count
+Write-Output $error[0]
+
+Stop-Transcript
