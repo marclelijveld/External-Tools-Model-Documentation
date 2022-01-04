51bbb2b3cb886841bb0b4f59ada3c63280043d2a
Marc Lelijveld <38921736+marclelijveld@users.noreply.github.com>
2022-01-04 09:43:11 +0100

    Add files via upload

--- ModelDocumenter/ModelDocumenter/Properties/AssemblyInfo.cs
+++ ModelDocumenter/ModelDocumenter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ModelDocumenter")]
-[assembly: AssemblyDescription("Model documentor using a power bi datamodel as vertipaq file.")]
+[assembly: AssemblyDescription("Model documentor using a Power BI datamodel as vertipaq file.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Macaw")]
 [assembly: AssemblyProduct("ModelDocumenter")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
